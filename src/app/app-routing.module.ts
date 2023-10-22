1c2a5b0085657435ce0bd291727854f73e042de1
AnneEberhard <anne.eberhard@gmx.net>
2023-10-22 10:44:53 +0200

    component game added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
+  { path: '', component: StartScreenComponent },
   { path: 'imprint', component: ImprintComponent },
-  { path: '', component: StartScreenComponent }
+  { path: 'game', component: GameComponent }
 ];
 
 @NgModule({
