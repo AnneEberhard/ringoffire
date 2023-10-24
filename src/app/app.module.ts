1211d1caf50304d41076d024ce3a7e54c80e887b
AnneEberhard <anne.eberhard@gmx.net>
2023-10-24 10:45:33 +0200

    player component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImprintComponent,
     StartScreenComponent,
-    GameComponent
+    GameComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
